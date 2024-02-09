a4ec84e9c11733607c3b605b27c38f06125d3405
Cevalp <miroslav.plavec1@gmail.com>
2024-02-09 19:56:24 +0100

    Vytvorenie projektu a zakladneho okna

---
+++ PasswordGenerator/src/FrontEnd/MainFrame.java
@@ -0,0 +1,16 @@
+package FrontEnd;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame(){
+        this.setTitle("Password Generator");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(1000, 800);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        this.setVisible(true);
+    }
+
+}
